2a551b8380dc3a9ba2f4daca98c801fd8e7abe3b
Byeonghun Kim <kimbh0218@naver.com>
2024-03-04 15:37:11 +0900

    first commit

---
+++ Setting/src/Test.java
@@ -0,0 +1,14 @@
+
+
+// public class Test{
+
+//     public static void main(String[] args) {
+//         int sum = 0;
+        
+// for(int i=0; i<30; i++) {
+//     sum += score[i];
+// }
+// int avg = sum / 30;
+//     System.out.println(avg);
+//     }
+// }
---
+++ Setting/src/practice.java
@@ -0,0 +1,7 @@
+public class practice {
+    public static void main(String[] args) {
+        System.out.println("안녕하세연");
+        System.out.println("김병훈입니다.");
+        System.out.printf("오늘은 %d월 %d일 입니더. \n",1, 30);
+    }
+}
